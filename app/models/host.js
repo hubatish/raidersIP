c2afb56a68f364da203f07257a176a171d713c48
EC2 Default User <ec2-user@ip-172-31-44-70.us-west-2.compute.internal>
2016-09-11 02:50:02 +0000

    Got GET and UPDATE to work. Found a callback version of find

--- app/models/host.js
+++ app/models/host.js
@@ -26,4 +26,64 @@ var HostSchema = new mongoose.Schema({
 module.exports = mongoose.model('GameHost',HostSchema);
 */
 
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var url = "mongodb://localhost:27017/host";
+
+//Go Ahead and connect & sketchily initialize the db
+var db; 
+var collection;
+MongoClient.connect(url,function(err,database){
+    if(err){
+        console.log("Coudln't connect to mongo. Error"+err);
+    } else{
+        db = database;
+        collection = db.collection('hosts');
+        console.log("Connected to mongo, db good to go");
+    }
+});
+
+var createHost = function(newHost,next){
+    console.log("Er, what is global db? "+db);
+    console.log("Adding host");
+
+    collection.update({internalIP:newHost.interntalIP,externalIP:newHost.externalIP},newHost,{upsert:true},function(err,result){
+        if(err)
+            next(err);
+        else{
+            console.log("Successful insert.");
+            next(null,result);
+        }
+    });
+}
+
+var deleteHost = function(host,next){
+    collection.remove(
+        {internalIP:host.internalIP,externalIP:host.externalIP},
+    function(err,result){
+        if(err)
+            next(err);
+        else
+            next(null,result);
+    });
+}
+
+var getAllHosts = function(next){
+    var hostCursor = collection.find();
+    hostCursor.toArray(function(err,allHosts){
+        if(err)
+            next(err);
+        else{
+            console.log("Number of hosts: "+allHosts.length);
+            next(null,allHosts);
+        }
+    });
+}
+
+module.exports = 
+{
+    createHost:createHost,
+    deleteHost:deleteHost,
+    getAllHosts:getAllHosts
+};
 
--- routes.js
+++ routes.js
@@ -13,7 +13,7 @@ router.route('/host')
     //create a host
     .post(function(req,res) {
         console.log("Beging processing a POST Host request");
-        var host = new GameHost();
+        /*var host = new GameHost();
         host.name = req.body.name;
         console.log("Made a host: "+ host);
         host.save(function(err){
@@ -21,6 +21,29 @@ router.route('/host')
             if(err)
                 res.send(err);
             res.json({message: "Host created!"});
+        });*/
+        var newHost = {
+            name:req.body.name,
+            internalIP:req.body.internalIP,
+            externalIP:req.body.externalIP
+        };
+        GameHost.createHost(newHost,function(err){
+            if(err){
+                console.log("error adding",err);
+                res.json({success:false,message:err});
+            }
+            else{
+                res.json({success:true,message:"Host created"});
+            }
+        });
+    })
+    //Get all the hosts (for debugging)
+    .get(function(req,res){
+        console.log("Starting to get all the hosts");
+        GameHost.getAllHosts(function(err,hosts){
+            if(err)
+                res.send(err);
+            res.json(hosts);
         });
     });
 
